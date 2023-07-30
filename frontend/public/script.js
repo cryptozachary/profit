2639081d2f73de15e7c72a20b4b968d176714e8c
Zachary Lipscomb <zachlipscomb86@gmail.com>
2023-07-30 11:25:59 -0700

    basic functionality with 3 formulas

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,18 +1,9 @@
 document.getElementById('checkProfitability').addEventListener('click', async () => {
     console.log('clicked')
 
+
     const asset = document.getElementById('asset').value;
     const formula = document.getElementById('formula').value;
-
-
-    const resultElement = document.getElementById('result');
-    if (data.isProfitable === 'rise') {
-        resultElement.textContent = "The asset is likely to increase in price!";
-    } else if (data.isProfitable === 'fall') {
-        resultElement.textContent = "The asset is likely to decrease in price!";
-    } else {
-        resultElement.textContent = "The asset's price movement is uncertain.";
-    }
 
     try {
         const response = await fetch('/check-profitability', {
@@ -20,16 +11,23 @@ document.getElementById('checkProfitability').addEventListener('click', async ()
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ asset, formula })
+            body: JSON.stringify({ cryptoAsset: asset, formulaType: formula })
         });
-        const data = await response.json();
 
-        const resultElement = document.getElementById('result');
-        if (data.isProfitable) {
-            resultElement.textContent = "The asset is likely to increase in price!";
+        if (response.ok) {
+            const data = await response.json();
+            const resultElement = document.getElementById('result');
+            if (data.isProfitable === 'rise') {
+                resultElement.textContent = "The asset is likely to increase in price!";
+            } else if (data.isProfitable === 'fall') {
+                resultElement.textContent = "The asset is likely to decrease in price!";
+            } else {
+                resultElement.textContent = "The asset's price movement is uncertain.";
+            }
         } else {
-            resultElement.textContent = "The asset is likely to decrease in price!";
+            console.error('Server returned an error:', await response.text());
         }
+
     } catch (error) {
         console.error("Error:", error);
     }
